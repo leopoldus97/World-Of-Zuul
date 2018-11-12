d8ccc1b9ec6faf4e6117920f657b1c96343cfa42
schemabuoi <43758232+schemabuoi@users.noreply.github.com>
2018-11-12 21:37:38 +0100

    Adding look command
    Adding command responsible for printing out our current locations info once again

--- ReallyGoodZuul/src/Game.java
+++ ReallyGoodZuul/src/Game.java
@@ -141,6 +141,9 @@ public class Game
         else if (commandWord.equals("quit")) {
             wantToQuit = quit(command);
         }
+        else if(commandWord.equals("look")) {
+            printLocationInfo();
+        }
 
         return wantToQuit;
     }
